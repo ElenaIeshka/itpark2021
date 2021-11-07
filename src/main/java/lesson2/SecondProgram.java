c8f8ee2ff4f96393a74e0c27693959e360fefc43
ElenaIeshka <Utu_06@mail.ru>
2021-11-07 18:54:09 +0300

    lesson 2 Простые арифм. операции и вызов методов. + дз

--- src/main/java/lesson2/SecondProgram.java
+++ src/main/java/lesson2/SecondProgram.java
@@ -1,6 +1,6 @@
 package lesson2;
 
-public class FirstProgram {
+public class SecondProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
@@ -18,28 +18,42 @@ public class FirstProgram {
         long result = multiplyAndSumma(5);
         System.out.println(result);
 
+        long fa = archimedesPrinciple(90);
+        System.out.println(fa);
 
 
     }
 
     /**
      * Метод будет складывать 2 аргумента и возвращать результат этой операции
+     *
      * @param arg1 первый аргумент
      * @param arg2 второй аргумент
      * @return результат сложения двух чисел
      */
-    public static long calculate (int arg1,  int arg2) {
+    public static long calculate(int arg1, int arg2) {
         return arg1 + arg2;
     }
 
-    public static long minus (int arg1,  int arg2) {
+    public static long minus(int arg1, int arg2) {
         return arg1 - arg2;
     }
 
 
-    public static long multiplyAndSumma (int arg1) {
+    public static long multiplyAndSumma(int arg1) {
         int result = 3 * arg1 + 15;
         result = result - 38;
         return result;
+    }
+
+
+    /**
+     * - Расчёт силы Архимеда в условиях Земли при погружении в воду
+     *
+     * @param arg1 Объём части тела, погруженной в жидкость или газ, м3
+     * @return Сила Архимеда, Н
+     */
+    public static long archimedesPrinciple(int arg1) {
+        return 1000L * arg1 * 10;
     }
 }
