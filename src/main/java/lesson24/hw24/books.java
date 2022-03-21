b08737055f7d6d591167cbea07741e0746ace365
ElenaIeshka <utu_06@mail.ru>
2022-03-21 22:22:31 +0300

    BD

---
+++ src/main/java/lesson24/hw24/books.java
@@ -0,0 +1,11 @@
+package lesson24.hw24;
+
+//import lombok.Data;
+//
+//@Data
+public class books {
+    //   private static
+//    private String isbn;
+//    private String title;
+//    private String author;
+}
