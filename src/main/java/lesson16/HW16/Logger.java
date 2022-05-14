2e3b7cff87a7deb87543b4e1549a6f71e6bbe195
ElenaIeshka <utu_06@mail.ru>
2022-05-14 13:51:28 +0300

    Hw16

---
+++ src/main/java/lesson16/HW16/Logger.java
@@ -0,0 +1,48 @@
+package lesson16.HW16;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+
+@RequiredArgsConstructor
+public class Logger implements Runnable{
+
+    private static final String FORMAT_TEXT = "%s %s %s %s\n";
+    private static final DateTimeFormatter FULL_DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+
+    private final LogLevel level;
+    private final String fileName;
+    private final long currentTimeMillis = System.currentTimeMillis();
+    private int counter = 1;
+
+
+
+    @Override
+    public void run(){
+        System.out.printf("Начал работу поток %s\n", Thread.currentThread().getName());
+        while (System.currentTimeMillis()<= currentTimeMillis + 60*1000){//время работы потока ограничено 1 минутой
+            try(FileWriter fw = new FileWriter(fileName,true)){
+                fw.write(generateString());
+                Thread.sleep(new Random().nextInt(5_000));
+            }catch (IOException | InterruptedException exception){
+                exception.printStackTrace();
+                break;
+            }
+
+        }
+
+    }
+    private String generateString(){
+        LocalDateTime now =LocalDateTime.now();
+        return String.format(FORMAT_TEXT,FULL_DATE_TIME_FORMAT.format(now),
+                level, Thread.currentThread().getName(),"Записали сообщение под № " + counter++);
+    }
+
+
+}
